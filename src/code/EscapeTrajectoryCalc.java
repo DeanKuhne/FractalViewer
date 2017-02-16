5919c7ddcbaf7cfe6180365e0819d1454a87b14b
ChangHoSung <changhos@buffalo.edu>
2017-02-15 19:17:26 -0500

    dfgdgj

--- src/code/EscapeTrajectoryCalc.java
+++ src/code/EscapeTrajectoryCalc.java
@@ -24,11 +24,13 @@ public class EscapeTrajectoryCalc {
 	// results should be returned as a 2-d array on int.
 	
 	public void BuriningShipSet(int xCalc, int yCalc){
+		double vertX;
+		double vertY;
 		for(double xCoor = -1.8; xCoor <= -1.7; xCoor = xCoor + 0.00000000000000001){
-			double vertX = (xCalc * xCalc) - (yCalc * yCalc) + xCoor;
+			vertX = (xCalc * xCalc) - (yCalc * yCalc) + xCoor;
 		}
 		for(double yCoor = -0.08; yCoor <= 0.025; yCoor = yCoor + 0.0000000000000001){
-			double vertY = Math.abs(2 * xCalc * yCalc) + yCoor;	
+			vertY = Math.abs(2 * xCalc * yCalc) + yCoor;	
 		}
 	}
 	
