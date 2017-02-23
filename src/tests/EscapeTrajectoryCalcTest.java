53c6153bab0fb3532a43f4f0dec1a2c90e336db3
dekuh <dekuh@WINDOWS-A2RHPL0>
2017-02-23 13:19:01 -0500

    Credit to Kyle, wrote coord test class using same logic as class that would return an array.

--- src/tests/EscapeTrajectoryCalcTest.java
+++ src/tests/EscapeTrajectoryCalcTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import code.EscapeTrajectoryCalc;
+import code.coordCalc;
 
 public class EscapeTrajectoryCalcTest {
 
@@ -31,9 +32,13 @@ public class EscapeTrajectoryCalcTest {
 
 	@Test
 	public void testEscDistOne() {
+		coordCalc frac = new coordCalc();
 		// Calculates the escape time for a coordinate whose distance from the
 		// origin exceeds the escape distance after a single loop pass [3 * 6
 		// points = 18 points]
+		assertEquals(255, frac.escTimeCoords(0.5946289062500001, 1.2949218750000122, 1), .01);// mandelbrot
+		assertEquals(255, frac.escTimeCoords(1.6933593749999853, 0.9765625, 2), .01);// julia
+		assertEquals(255, frac.escTimeCoords(0.9921875, 1.05625, 4), .01);// multibrot
 
 	}
 
