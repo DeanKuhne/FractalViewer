fd6f00de243f46fc693220be1e5ca5eb3caa6601
dekuh <dekuh@WINDOWS-A2RHPL0>
2017-03-14 19:35:13 -0400

    what error is this???

--- src/code/UIApplication.java
+++ src/code/UIApplication.java
@@ -1,11 +1,9 @@
 package code;
-
 public class UIApplication {
-
-	public static void main(String[] args) {
-		UI ui = new UI();
-		ui.setBounds(0, 0, 515, 560);
-		ui.setVisible(true);
-		ui.setResizable(false);
-	}
+    public static void main(String[] args) {
+    	UI ui = new UI();
+        ui.setBounds(100, 100, 600, 300);
+        ui.setVisible(true);
+        ui.setResizable(false);
+    }
 }
