8fdca6f79d97e31919770d7632ddf68fb686549d
ChangHoSung <changhos@buffalo.edu>
2017-03-14 19:57:25 -0400

    s

--- src/code/UIApplication.java
+++ src/code/UIApplication.java
@@ -1,22 +1,14 @@
 package code;
-<<<<<<< HEAD
 
 public class UIApplication {
-
 	public static void main(String[] args) {
 		UI ui = new UI();
 		ui.setBounds(0, 0, 515, 560);
 		ui.setVisible(true);
 		ui.setResizable(false);
 	}
-<<<<<<< HEAD
-
-
 }
-=======
-}
->>>>>>> branch 'master' of https://github.com/CSE116-Spring2017/semesterlongproject-b5-bitsplease.git
-=======
+
 public class UIApplication {
     public static void main(String[] args) {
     	UI ui = new UI();
@@ -25,4 +17,3 @@ public class UIApplication {
         ui.setResizable(false);
     }
 }
->>>>>>> branch 'master' of https://github.com/CSE116-Spring2017/semesterlongproject-b5-bitsplease.git
