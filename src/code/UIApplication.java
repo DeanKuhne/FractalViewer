10614a9b620b0ef5aaa1140df71a778957589dfe
dekuh <dekuh@WINDOWS-A2RHPL0>
2017-03-14 18:30:14 -0400

    testing error logs

--- src/code/UIApplication.java
+++ src/code/UIApplication.java
@@ -1,6 +1,11 @@
 package code;
 
 public class UIApplication {
-	UI ui = new UI();
 
+	public static void main(String[] args) {
+		UI ui = new UI();
+		ui.setBounds(0, 0, 515, 560);
+		ui.setVisible(true);
+		ui.setResizable(false);
+	}
 }
