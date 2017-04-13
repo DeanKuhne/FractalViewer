e3ebd717b2e11ab1a091cb7f024f8454f1d8a956
changhos <changhos@jon-snow.cse.buffalo.edu>
2017-04-12 20:04:25 -0400

    s

--- src/code/UIApplication.java
+++ src/code/UIApplication.java
@@ -1,21 +1,10 @@
 package code;
 
-/*public class UIApplication {
-	public static void main(String[] args) {
-		UI ui = new UI();
-		ui.setBounds(0, 0, 542, 585);
-		ui.setVisible(true);
-	}
-<<<<<<< HEAD
-}*/
-
 public class UIApplication {
     public static void main(String[] args) {
     	UI ui = new UI();
-        ui.setBounds(100, 100, 600, 300);
+        ui.setBounds(100, 100, 542, 585);
         ui.setVisible(true);
         ui.setResizable(false);
     }
 }
-//=======
-//>>>>>>> branch 'master' of https://github.com/CSE116-Spring2017/semesterlongproject-b5-bitsplease.git
